162228b9f94d8ce38b0e2f5b6cad6c3032676d39
arian <arian.maki@yahoo.com>
2025-09-14 16:47:28 +0200

    useReducer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,46 @@
 import "./App.css";
 import AddNewNote from "./components/AddNewNote";
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import NoteList from "./components/NoteList";
 import NoteStatus from "./components/NoteStatus";
 import NoteHeader from "./components/NoteHeader";
 
 function App() {
-  //! States
-  const [notes, setNotes] = useState([]);
+  function notesReducer(notes, { type, payload }) {
+    switch (type) {
+      case "add": {
+        return [...notes, payload];
+      }
+      case "delete": {
+        return notes.filter((note) => note.id !== payload);
+      }
+      case "complete": {
+        return notes.map((note) =>
+          note.id === payload
+            ? { ...note, isCompleted: !note.isCompleted }
+            : note
+        );
+      }
+      default:
+        throw new Error("Unknown error" + type);
+    }
+  }
+
+  //! Hooks
+  const [notes, dispatch] = useReducer(notesReducer, []);
   const [sortBy, setSortBy] = useState("");
 
   //! Handlers
-  const handleAddNote = (newNote) => setNotes([...notes, newNote]);
+  const handleAddNote = (newNote) =>
+    dispatch({ type: "add", payload: newNote });
 
   const handleDeleteNote = (id) => {
-    setNotes(notes.filter((n) => n.id !== id));
+    dispatch({ type: "delete", payload: id });
   };
 
   const handleCompleteNote = (e) => {
     const noteId = Number(e.target.value);
-    setNotes((prevNotes) =>
-      prevNotes.map((note) =>
-        note.id === noteId ? { ...note, isCompleted: !note.isCompleted } : note
-      )
-    );
+    dispatch({ type: "complete", payload: noteId });
   };
 
   // Return notes; sorted according to the current sort option
